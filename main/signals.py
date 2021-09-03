e4eb8b81d7afbee535662c250b62df2dfb78c5f0
Jackam-spec <69396901+Jackam-spec@users.noreply.github.com>
2021-09-03 20:49:51 +0300

    first commit

---
+++ main/signals.py
@@ -0,0 +1,35 @@
+from io import BytesIO
+import logging
+from PIL import Image
+from django.core.files.base import ContentFile
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import ProductImage
+
+# Generate Thumbnails (Signal)
+THUMBNAIL_SIZE = (300, 300)
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(pre_save, sender=ProductImage)
+def generate_thumbnail(sender, instance, **kwargs):
+    logger.info(
+        "Generating thumbnail for product %d",
+        instance.product.id,
+    )
+    image = Image.open(instance.image)
+    image = image.cnvert("RGB")
+    image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
+
+    temp_thumb = BytesIO()
+    image.save(temp_thumb, "JPEG")
+    temp_thumb.seek(0)
+
+    # Save is set to false to prevent running an infinite loop
+    instance.thumbnail.save(
+        instance.image.name,
+        ContentFile(temp_thumb.read()),
+        save=False,
+    )
+    temp_thumb.close()
---
+++ main/tests/tests_view.py
@@ -0,0 +1,27 @@
+from django.forms import forms
+from django.test import TestCase
+from django.urls import reverse
+from main import forms
+
+# Create your tests here.
+
+
+class TestPage(TestCase):
+    def test_home_page_works(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'BookTime')
+
+    def test_about_us_page_works(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+        self.assertContains(response, 'BookTime')
+
+    def test_contact_us_page_works(self):
+        response = self.client.get(reverse('contact_us'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact-form.html')
+        self.assertContains(response, 'BookTime')
+        self.assertIsInstance(response.context['form'], forms.ContactForm)
